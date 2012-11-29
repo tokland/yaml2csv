c6ee634ddbf66932a7a237ed1ddd2ab463dbab23
Arnau Sanchez <pyarnau@gmail.com>
2012-11-29 14:47:14 +0100

    update to Ruby 1.9

--- lib/yaml2csv.rb
+++ lib/yaml2csv.rb
@@ -10,24 +10,24 @@ Yaml2csv.extend_hash
 module Yaml2csv
   # Convert a string containing YAML data to a CSV string
   def self.yaml2csv(yamldata, options = {})
+    options = options.update(:field_separator => ',')
     hash = YAML::load(yamldata)
-    output = ""
-    csv_writer = CSV::BasicWriter.new(output, options[:field_separator])
-    hash.to_enum(:walk).each do |path, key, value|
-      strvalue = value.is_a?(String) ? value : value.inspect
-      csv_writer << [path.join("/"), key, strvalue]
+    CSV.generate(:col_sep => options[:field_separator]) do |csv|
+      hash.to_enum(:walk).each do |path, key, value|
+        strvalue = value.is_a?(String) ? value : value.inspect
+        csv << [path.join("/"), key, strvalue]
+      end
     end
-    output
   end
   
   # Convert a string containing CSV values to a YAML string
   def self.csv2yaml(csvdata, options = {})
-    rows = CSV::parse(csvdata, options[:field_separator])
+    options = options.update(:field_separator => ',')
+    rows = CSV.parse(csvdata, :col_sep => options[:field_separator])
     walk_array = rows.map do |strpath, key, value| 
       [strpath.split("/").map(&:to_s), key.to_s, value.to_s]
     end
     hash = Hash.unwalk_from_array(walk_array)
-    $KCODE = "UTF8"
     # Remove any trailing spaces inserted by String#to_yaml
     hash.ya2yaml.gsub(/\s+$/, '') + "\n"
   end  
